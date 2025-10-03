7015b148724c2a58e786e64c31ceea8ed3e65b12
BekoLolek <57350086+BekoLolek@users.noreply.github.com>
2025-10-03 21:18:18 +0200

    load data tweaks

--- js/loadData.js
+++ js/loadData.js
@@ -18,57 +18,57 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
 
                 const memberHTML = `
-                    <div class="team-member row">
+    <div class="team-member row">
 
-    <!-- NAME -->
-    <div class="col-lg-12 order-lg-1 member-name">
-        <h2>${person.name}</h2>
-    </div>
-
-    <!-- IMAGE -->
-    <div class="col-lg-6 order-lg-2 text-center member-image">
-        <img src="${person.image}" class="img-fluid" alt="${person.name}">
-    </div>
-
-    <!-- DESCRIPTION -->
-    <div id="${person.contact.email}_desc_id" style="display:none;" class="col-lg-12 order-lg-3 team-text member-desc">
-        <p>${person.description}</p>
-    </div>
-
-    <!-- BUTTON -->
-    <div class="col-lg-12 order-lg-4 member-spec">
-        <ul>
-            ${person.specializations.map(spec => `<li>${spec}</li>`).join('')}
-        </ul>
-        <div class="button-container">
-            <img src="/img/site/buttonbuborek.png" alt="bubi">
-            <button class="member-button" onclick='
-                var x = document.getElementById("${person.contact.email}_desc_id");
-                if (x.style.display === "none") {
-                    x.style.display = "block";
-                } else {
-                    x.style.display = "none";
-                }
-            '>${person.buttonText}</button>
+        <!-- NAME -->
+        <div class="col-12 order-1 member-name">
+            <h2>${person.name}</h2>
         </div>
-    </div>
 
-    <!-- AVAILABILITY -->
-    <div class="col-lg-6 order-lg-5 member-time">
-        <p class="member-set-text">Rendelési idő:<br> 
-            <span class="member-unset-text">${person.availability}</span>
-        </p>
-    </div>
+        <!-- IMAGE -->
+        <div class="col-12 col-lg-6 order-2 text-center member-image">
+            <img src="${person.image}" class="img-fluid" alt="${person.name}">
+        </div>
 
-    <!-- CONTACT -->
-    <div class="col-lg-6 order-lg-6 member-contact">
-        <p class="member-set-text">Elérhetőségek:</p>
-        <p class="member-unset-text">${person.contact.phone}<br>${person.contact.email}</p>
-    </div>
+        <!-- BUTTON (MOBILE: right after image, DESKTOP: after description) -->
+        <div class="col-12 order-3 order-lg-4 member-spec">
+            <ul>
+                ${person.specializations.map(spec => `<li>${spec}</li>`).join('')}
+            </ul>
+            <div class="button-container">
+                <img src="/img/site/buttonbuborek.png" alt="bubi">
+                <button class="member-button" onclick='
+                    var x = document.getElementById("${person.contact.email}_desc_id");
+                    if (x.style.display === "none") {
+                        x.style.display = "block";
+                    } else {
+                        x.style.display = "none";
+                    }
+                '>${person.buttonText}</button>
+            </div>
+        </div>
 
-</div>
-                    
-                `;
+        <!-- DESCRIPTION -->
+        <div id="${person.contact.email}_desc_id" style="display:none;" class="col-12 order-4 order-lg-3 team-text member-desc">
+            <p>${person.description}</p>
+        </div>
+
+        <!-- AVAILABILITY -->
+        <div class="col-12 col-lg-6 order-5 member-time">
+            <p class="member-set-text">Rendelési idő:<br> 
+                <span class="member-unset-text">${person.availability}</span>
+            </p>
+        </div>
+
+        <!-- CONTACT -->
+        <div class="col-12 col-lg-6 order-6 member-contact">
+            <p class="member-set-text">Elérhetőségek:</p>
+            <p class="member-unset-text">${person.contact.phone}<br>${person.contact.email}</p>
+        </div>
+
+    </div>
+`;
+
                 count++;
                 // Append the created HTML to the container
                 container.innerHTML += memberHTML;
